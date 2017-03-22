3b0bb131dfa88e96cfe359bcea5382a771dbc6a6
weiwang314 <wei.wang@wbdevops.com>
2017-03-22 15:09:32 -0700

    get role request handler

---
+++ tests/GetRoleRequestHandlerTest.php
@@ -0,0 +1,82 @@
+<?php
+declare(strict_types = 1);
+
+namespace Gdbots\Iam;
+
+use Acme\Schemas\Iam\Node\RoleV1;
+use Acme\Schemas\Iam\Request\GetRoleRequestV1;
+use Gdbots\Pbj\WellKnown\Microtime;
+use Gdbots\Tests\Iam\AbstractPbjxTest;
+
+class GetRoleRequestHandlerTest extends AbstractPbjxTest
+{
+    public function testHandleRequest()
+    {
+        $this->createRole();
+
+        $request = GetRoleRequestV1::fromArray([
+            'node_ref' => 'acme:role:8695f644-0e7f-11e7-93ae-92361f002671'
+        ]);
+
+        $handler = new GetRoleRequestHandler($this->ncr);
+        $response = $handler->handleRequest($request, $this->pbjx);
+        $responseRole = $response->get('node');
+
+        $this->assertInstanceOf('Acme\Schemas\Iam\Node\RoleV1', $responseRole);
+        $this->assertSame(
+            '8695f644-0e7f-11e7-93ae-92361f002671',
+            $responseRole->get('_id')->toString()
+        );
+
+        $this->assertSame(
+            ['acme:blog:command:*', 'acme:video:command:*'],
+            $responseRole->get('allowed')
+        );
+
+        $this->assertSame(
+            ['acme:blog:command:publish-article'],
+            $responseRole->get('denied')
+        );
+    }
+
+    /**
+     * @expectedException Gdbots\Ncr\Exception\NodeNotFound
+     * @expectedExceptionMessage No method available to find role.
+     */
+    public function testHandleNodeRefNotFound()
+    {
+        $request = GetRoleRequestV1::fromArray([
+            'not_node_ref' => 'acme:role:8695f644-0e7f-11e7-93ae-92361f002671'
+        ]);
+
+        $handler = new GetRoleRequestHandler($this->ncr);
+        $handler->handleRequest($request, $this->pbjx);
+    }
+
+    /**
+     * @expectedException Gdbots\Ncr\Exception\NodeNotFound
+     */
+    public function testHandleNodeNotFound()
+    {
+        $request = GetRoleRequestV1::fromArray([
+            'node_ref' => 'acme:role:wrong-node-ref'
+        ]);
+
+        $handler = new GetRoleRequestHandler($this->ncr);
+        $handler->handleRequest($request, $this->pbjx);
+    }
+
+    /**
+     *  create a role and put it to ncr
+     */
+    private function createRole(): void
+    {
+        $node = RoleV1::fromArray(['_id' => '8695f644-0e7f-11e7-93ae-92361f002671']);
+        $node
+            ->addToSet('allowed', ['acme:blog:command:*', 'acme:video:command:*'])
+            ->addToSet('denied', ['acme:blog:command:publish-article'])
+            ->set('updated_at', Microtime::create());
+
+        $this->ncr->putNode($node, null);
+    }
+}
